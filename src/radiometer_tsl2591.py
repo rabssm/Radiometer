8c7e6ac8a40f57b7806699ef19970dce049b1a98
Richard <>
2022-11-23 11:39:31 +0000

    Modifed to get lux, vis and ir values.

--- src/radiometer_tsl2591.py
+++ src/radiometer_tsl2591.py
@@ -44,7 +44,7 @@ class RadiometerDataLogger():
 # Add a get_light_levels method to the afruit_tsl2591 class
 
 
-class afruit_tsl2591_extended():
+class adafruit_tsl2591_extended(adafruit_tsl2591.TSL2591):
 
     def get_light_levels(self):
         """Read the sensor and calculate a lux value from both its infrared
@@ -60,10 +60,10 @@ class afruit_tsl2591_extended():
         atime = 100.0 * self._integration_time + 100.0
 
         # Set the maximum sensor counts based on the integration time (atime) setting
-        if self._integration_time == self.INTEGRATIONTIME_100MS:
-            max_counts = self._TSL2591_MAX_COUNT_100MS
+        if self._integration_time == adafruit_tsl2591.INTEGRATIONTIME_100MS:
+            max_counts = adafruit_tsl2591._TSL2591_MAX_COUNT_100MS
         else:
-            max_counts = self._TSL2591_MAX_COUNT
+            max_counts = adafruit_tsl2591._TSL2591_MAX_COUNT
 
         # Handle overflow.
         if channel_0 >= max_counts or channel_1 >= max_counts:
@@ -75,17 +75,17 @@ class afruit_tsl2591_extended():
         # Calculate lux using same equation as Arduino library:
         #  https://github.com/adafruit/Adafruit_TSL2591_Library/blob/master/Adafruit_TSL2591.cpp
         again = 1.0
-        if self._gain == self.GAIN_MED:
+        if self._gain == adafruit_tsl2591.GAIN_MED:
             again = 25.0
-        elif self._gain == self.GAIN_HIGH:
+        elif self._gain == adafruit_tsl2591.GAIN_HIGH:
             again = 428.0
-        elif self._gain == self.GAIN_MAX:
+        elif self._gain == adafruit_tsl2591.GAIN_MAX:
             again = 9876.0
-        cpl = (atime * again) / self._TSL2591_LUX_DF
-        lux1 = (channel_0 - (self._TSL2591_LUX_COEFB * channel_1)) / cpl
+        cpl = (atime * again) / adafruit_tsl2591._TSL2591_LUX_DF
+        lux1 = (channel_0 - (adafruit_tsl2591._TSL2591_LUX_COEFB * channel_1)) / cpl
         lux2 = (
-            (self._TSL2591_LUX_COEFC * channel_0) -
-            (self._TSL2591_LUX_COEFD * channel_1)
+            (adafruit_tsl2591._TSL2591_LUX_COEFC * channel_0) -
+            (adafruit_tsl2591._TSL2591_LUX_COEFD * channel_1)
         ) / cpl
         return max(lux1, lux2), channel_0, channel_1
 
@@ -98,12 +98,13 @@ if __name__ == "__main__":
 
     # Open the sensor
     i2c = board.I2C()
-    sensor = adafruit_tsl2591.TSL2591(i2c)
+    sensor = adafruit_tsl2591_extended(i2c)
 
     # Set max gain and fastest integration time (100ms)
     sensor.enable()
     sensor.gain = adafruit_tsl2591.GAIN_MAX
     sensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_100MS
+    prev_lux = -100
     time.sleep(0.5)
 
     radiometer_data_logger = RadiometerDataLogger()
@@ -111,21 +112,23 @@ if __name__ == "__main__":
     while True:
         try:
             # Read and calculate the light level in lux.
-            lux = sensor.lux
+            lux, vis_level, ir_level = sensor.get_light_levels()
             if sensor.gain != adafruit_tsl2591.GAIN_MAX and lux < 3.0:
                 sensor.disable()
                 sensor.gain = adafruit_tsl2591.GAIN_MAX
                 sensor.enable()
 
-            # ir, visible = sensor.raw_luminosity()
-
             time_stamp = datetime.datetime.now()
             if DEBUG:
-                print(time_stamp.strftime("%d/%m/%Y %H:%M:%S.%f")
-                      [:-3] + " " + "Total light: {0} lux".format(lux))
+                out_string = '{0:s} {1:.9f} {2:d} {3:d}'.format(time_stamp.strftime("%Y/%m/%d %H:%M:%S.%f")[:-3], lux, vis_level, ir_level)
+                print(out_string)
             radiometer_data_logger.log_data(time_stamp, lux)
+
+            prev_lux = lux
             time.sleep(0.05)
-        except:
+
+        except Exception as e:
+            # print(e)
             # print("Error reading from sensor")
             if sensor.gain == adafruit_tsl2591.GAIN_MAX:
                 sensor.disable()
