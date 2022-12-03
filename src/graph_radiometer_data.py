d9110bbf000d21ce6d1458c13bb7f8643a4549e2
Richard <>
2022-12-03 12:32:10 +0000

    Added -n option to limit graph range to night readings.

--- src/graph_radiometer_data.py
+++ src/graph_radiometer_data.py
@@ -7,9 +7,13 @@ from matplotlib import pyplot as plt
 
 ap = argparse.ArgumentParser(description='Analyse radiometer data')
 ap.add_argument("file", type=str, help="File or directory to analyse.")
+ap.add_argument("-n", "--night", action='store_true', help="Display with night readings range")
+
 args = vars(ap.parse_args())
 
 file_name = args['file']
+night_range = args['night']
+
 print(file_name)
 
 columns = ["Date", "Time", "Lux", "Visible", "IR", "Gain"]
@@ -22,5 +26,7 @@ print(df)
 plt.plot(times, df.Lux)
 plt.xlabel('Time')
 plt.ylabel('Lux')
+if night_range:
+    plt.ylim(-0.1, 0.5)
 
 plt.show()
