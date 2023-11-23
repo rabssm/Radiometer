2fe3338a951bc13c0590bf812eea44221622e08a
Richard <>
2023-11-23 13:53:56 +0000

    Updates based on radiometer_tsl2591.py.

--- src/sqm_tsl2591.py
+++ src/sqm_tsl2591.py
@@ -5,15 +5,21 @@ import signal
 import threading
 import time
 import numpy as np
+from collections import deque
+import syslog
 import board
+from adafruit_extended_bus import ExtendedI2C as I2C
 import adafruit_tsl2591
 
 DATA_DIR = os.path.expanduser('~/radiometer_data/')
+SQM_FILE = '/tmp/sqm_tsl2591.txt'
 
-DEBUG = True
 
 # Minimum time to wait after a sensor time or gain setting
-GUARD_TIME = 1.3
+GUARD_TIME = 0.12
+
+# The tsl2591 default i2c address is 0x29
+DEFAULT_I2C_ADDRESS = adafruit_tsl2591._TSL2591_ADDR
 
 
 def signalHandler(signum, frame):
@@ -21,17 +27,53 @@ def signalHandler(signum, frame):
     os._exit(0)
 
 
+def reset_sensor(sensor, gain, integration_time):
+    # Sensor reset
+    if verbose:
+        print("Resetting sensor")
+    # sensor.disable()
+    sensor.reset()
+    sensor.gain = gain
+    sensor.enable()
+    sensor.integration_time = integration_time
+    # Wait for next interrupt
+    sensor.wait_interrupt()
+
+
+class Sqm_Writer():
+    def __init__(self):
+        self.rolling = deque(maxlen=10)
+
+
+    def update(self, sky_brightness):
+        # Take a rolling average over last 10 measurements (6s) and write it to /tmp/sqm_tsl2591.txt
+
+        self.rolling.append(sky_brightness)
+        rolling_average = np.average(self.rolling)
+
+        with open(SQM_FILE, 'w') as sqm_file:
+            sqm_file.write(str(rolling_average) + "\n")
+
+        return
+
 # Class for logging detections to radiometer data file
 class RadiometerDataLogger():
 
-    def __init__(self):
+    def __init__(self, name=""):
+        self.name = name
+        if name:
+            self.name = "_" + name + "_"
         # Make the data logging directory
         os.makedirs(DATA_DIR, exist_ok=True)
 
         # Set the filename and open it for appending
-        self.filename = "R" + datetime.datetime.now().strftime("%Y%m%d") + ".csv"
+        self.filename = "R" + self.name + \
+            datetime.datetime.now().strftime("%Y%m%d") + ".csv"
+        if verbose:
+            print("Writing data to file:", DATA_DIR + self.filename)
         self.rmfile = open(DATA_DIR + self.filename, "a")
 
+        # Start a thread to periodically flush the data to disk
         self.flush_thread = threading.Thread(target=self.flush_file)
         self.flush_thread.start()
 
@@ -39,7 +81,7 @@ class RadiometerDataLogger():
     def log_data(self, obs_time, lux_value, vis_level, ir_level, again, atime):
         # Check for date change
         try:
-            filename = "R" + obs_time.strftime("%Y%m%d") + ".csv"
+            filename = "R" + self.name + obs_time.strftime("%Y%m%d") + ".csv"
             if filename != self.filename:
                 self.rmfile.close()
                 self.filename = filename
@@ -49,7 +91,7 @@ class RadiometerDataLogger():
             out_string = '{0:s} {1:.9f} {2:d} {3:d} {4:.1f} {5:.1f}\n'.format(obs_time.strftime(
                 "%Y/%m/%d %H:%M:%S.%f")[:-3], lux_value, vis_level, ir_level, again, atime)
             self.rmfile.write(out_string)
-            if DEBUG:
+            if verbose:
                 print(out_string, end='')
 
         except Exception as e:
@@ -68,7 +110,7 @@ class RadiometerDataLogger():
 # Add a get_light_levels method to the adafruit_tsl2591 class
 class adafruit_tsl2591_extended(adafruit_tsl2591.TSL2591):
 
-    def get_light_levels(self):
+    def get_light_levels(self, disable_exception=False):
         """Read the sensor and calculate a lux value from both its infrared
         and visible light channels.
 
@@ -93,7 +135,8 @@ class adafruit_tsl2591_extended(adafruit_tsl2591.TSL2591):
                 "Overflow reading light channels!, Try to reduce the gain of\n "
                 + "the sensor using adafruit_tsl2591.GAIN_LOW"
             )
-            raise RuntimeError(message)
+            if not disable_exception:
+                raise RuntimeError(message)
         # Calculate lux using same equation as Arduino library:
         #  https://github.com/adafruit/Adafruit_TSL2591_Library/blob/master/Adafruit_TSL2591.cpp
         again = 1.0
@@ -126,6 +169,19 @@ class adafruit_tsl2591_extended(adafruit_tsl2591.TSL2591):
             | adafruit_tsl2591._TSL2591_ENABLE_NPIEN,
         )
 
+    def reset(self):
+        # Perform a device reset
+        val = 0b10000000
+        # control = self._read_u8(adafruit_tsl2591._TSL2591_REGISTER_CONTROL)
+        # control &= 0b01111111
+        control = val
+
+        # A write to the reset register always generates an exception
+        try:
+            self._write_u8(adafruit_tsl2591._TSL2591_REGISTER_CONTROL, control)
+        except:
+            pass
+
     def clear_interrupts(self):
         # Clear ALS interrupts.
         with self._device as i2c:
@@ -137,11 +193,11 @@ class adafruit_tsl2591_extended(adafruit_tsl2591.TSL2591):
 
     def wait_interrupt(self):
         # Wait for AINT interrupt to signal a reading has completed
-        # Initial sleep 50ms
-        time.sleep(0.05)
+        # Initial sleep 500ms
+        time.sleep(0.5)
         # Get the status to check for the AINT interrupt being asserted
         while self._read_u8(0x13) & 0x10 == 0:
-            time.sleep(0.005)
+            time.sleep(0.05)
 
         self.clear_interrupts()
 
@@ -151,32 +207,68 @@ if __name__ == "__main__":
 
     # Construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser(description='Acquire light levels')
-
+    ap.add_argument("-a", "--address", type=lambda x: int(x, 0), default=DEFAULT_I2C_ADDRESS,
+                    help="Set the light sensor's i2c address. Default is " + hex(DEFAULT_I2C_ADDRESS))
+    ap.add_argument("-b", "--bus", type=int, default=1,
+                    help="Specify the i2c bus used for connecting the sensor e.g. 3 if /dev/i2c-3 has been created using dtoverlay. Default is bus 1")
+    gain_choices = ["max", "high", "med", "low", "auto"]
+    ap.add_argument(
+        "-g", "--gain", choices=gain_choices, type=str, default="auto", help="Gain level for the light sensor. Default is auto")
+    ap.add_argument("-m", "--multiplexer", type=int, default=None,
+                    help="Connect to the i2c sensor via an adafruit TCA9548A multiplexer using the number of the multiplexer channel e.g. 0-7")
+    ap.add_argument("-n", "--name", type=str, default="",
+                    help="Optional name of the sensor for the output file name. Default is no name")
     ap.add_argument("-v", "--verbose", action='store_true',
                     help="Verbose output to terminal")
-
     args = vars(ap.parse_args())
 
+    i2c_address = args['address']
+    i2c_bus = args['bus']
+    gain_name = args['gain']
+    device_name = args['name']
+    multiplexer = args['multiplexer']
     verbose = args['verbose']
 
+    # Get the TSL2591 gain from the command line string. If the gain is set to auto, set the gain to maximum
+    valid_device_gain_settings = [adafruit_tsl2591.GAIN_MAX,
+                                  adafruit_tsl2591.GAIN_HIGH, adafruit_tsl2591.GAIN_MED, adafruit_tsl2591.GAIN_LOW, adafruit_tsl2591.GAIN_MAX]
+    required_device_gain_setting = valid_device_gain_settings[gain_choices.index(
+        gain_name)]
+    auto_gain = True if gain_name == "auto" else False
+
+    # Handle termination signals gracefully
     signal.signal(signal.SIGINT, signalHandler)
     signal.signal(signal.SIGTERM, signalHandler)
 
-    # Open the sensor
-    i2c = board.I2C()
-    sensor = adafruit_tsl2591_extended(i2c)
+    # Open the i2c bus
+    i2c = I2C(i2c_bus)
 
-    # Set max gain and slowest integration time (600ms)
+    # Create the sensor or TCA9548A object and pass it the I2C bus
+    if multiplexer is not None:
+        import adafruit_tca9548a
+        tca = adafruit_tca9548a.TCA9548A(i2c)
+        # For the sensor on the multiplexer, create it using the TCA9548A channel instead of the I2C object
+        sensor = adafruit_tsl2591_extended(tca[multiplexer])
+    else:
+        sensor = adafruit_tsl2591_extended(i2c, address=i2c_address)
+
+    # Set gain and integration time (600ms)
     sensor.enable()
-    gain_level = adafruit_tsl2591.GAIN_MAX
+    gain_level = required_device_gain_setting
     sensor.gain = gain_level
+    # sensor.gain = adafruit_tsl2591.GAIN_LOW # for testing
     sensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_600MS
     prev_lux = -100
     saturation_counter = 0
 
-    time.sleep(GUARD_TIME)
+    time.sleep(0.5)
 
-    radiometer_data_logger = RadiometerDataLogger()
+    # Create the data logger
+    radiometer_data_logger = RadiometerDataLogger(name=device_name)
+
+    # Create the SQM readings writer
+    sqm_writer = Sqm_Writer()
+
 
     while True:
         try:
@@ -193,8 +285,11 @@ if __name__ == "__main__":
             radiometer_data_logger.log_data(
                 time_stamp, lux, vis_level, ir_level, again, atime)
 
+            # Write the latest SQM value
+            sqm_writer.update(np.log10(lux/108000)/-0.4)
+
             # Check if the gain level can be changed back to max
-            if gain_level != adafruit_tsl2591.GAIN_MAX and lux < 3.0:
+            if auto_gain and gain_level != adafruit_tsl2591.GAIN_MAX and lux < 1.0:
                 # sensor.disable()
                 sensor.adc_en_off()
                 gain_level = adafruit_tsl2591.GAIN_MAX
@@ -209,31 +304,48 @@ if __name__ == "__main__":
             saturation_counter = 0
             prev_lux = lux
 
-        # An exception can occur if the light sensor saturates, so change the gain.
-        # Gain at GAIN_MED will allow measurements to be taken up to about 2000 lux
+
         # An exception can occur if the light sensor saturates
         except Exception as e:
             if verbose:
                 print(e)
 
+            # If there is an exception with fixed gain, reset the sensor every 1200 readings (120s)
+            if not auto_gain:
+                saturation_counter += 1
+                if saturation_counter > 100:
+                    reset_sensor(sensor, gain_level,
+                                 adafruit_tsl2591.INTEGRATIONTIME_100MS)
+                    saturation_counter = 0
+                continue
+
             # Attempt to lower gain so that readings can continue
             # Gain at GAIN_MED will allow measurements to be taken up to about 3000 lux
             if gain_level == adafruit_tsl2591.GAIN_MAX:
+
+                # Log the sensor values anyway even though there has been an exception as the IR sensor values may still be useful.
+                if prev_lux > 0:
+                    lux, vis_level, ir_level, again, atime = sensor.get_light_levels(
+                        disable_exception=True)
+                    radiometer_data_logger.log_data(
+                        time_stamp, prev_lux, vis_level, ir_level, again, atime)
+
                 # sensor.disable()
                 sensor.adc_en_off()
                 gain_level = adafruit_tsl2591.GAIN_MED
                 sensor.gain = gain_level
                 sensor.enable()
-                sensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_600MS
+                sensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_100MS
                 # Sleep to ensure next reading is valid
                 sensor.wait_interrupt()
                 # time.sleep(GUARD_TIME)
 
-            # If the sensor has been saturated for a long time (120s), then stay asleep
+            # If the sensor has been saturated for a long time (60s), reset the sensor and then stay asleep
             else:
                 saturation_counter += 1
-                if saturation_counter > 1200:
-                    sensor.disable()
+                if saturation_counter > 100:
+                    reset_sensor(sensor, gain_level,
+                                 adafruit_tsl2591.INTEGRATIONTIME_100MS)
                     time.sleep(120)
 
                     # Take a reading at lowest gain, then set the gain back to medium
@@ -245,6 +357,10 @@ if __name__ == "__main__":
                         time_stamp = datetime.datetime.now()
                         radiometer_data_logger.log_data(
                             time_stamp, lux, vis_level, ir_level, again, atime)
+ 
+                        # Write the new SQM value
+                        sqm_writer.update(np.log10(lux/108000)/-0.4)
+
                     except Exception as e:
                         print(e)
 
